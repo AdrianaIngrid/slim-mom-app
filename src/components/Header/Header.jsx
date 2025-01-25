4aba71edcc29f8f4e3ec8c6504f59d08bd51fc30
AdrianaIngrid <150287447+AdrianaIngrid@users.noreply.github.com>
2025-01-25 15:46:13 +0200

    p

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { selectIsLoggedIn, selectUser } from "../AREDUX/Auth/selectors";
 import { logout } from "../AREDUX/Auth/operations";
-import logoImage from "../../Images/slimLogo.png";
+// import logoImage from "../../Images/slimLogo.png";
 import { NavLink } from "react-router-dom";
 import { useNavigate } from 'react-router-dom';
 
@@ -29,7 +29,7 @@ function Header() {
   console.log("Header Component: Rendered with", { isLoggedIn, user });
   return (
     <div className={css.headerDiv}>
-      <img  src={logoImage} alt="logoImageSlimMom" className={css.logoImg}/>
+      {/* <img  src={logoImage} alt="logoImageSlimMom" className={css.logoImg}/> */}
     <header className={css.headerH}>
       
       <nav>
